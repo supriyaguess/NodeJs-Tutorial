1b9a1fe4c9f015b63725c33e17888035e366a624
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2025-12-16 23:21:31 +0530

    added nodejs files for hello-world basic

---
+++ hello-world/math.js
@@ -0,0 +1,17 @@
+//function add(a,b) {
+//    return a + b;
+//}
+
+//function minus(a,b) {
+//    return a-b;
+//}
+
+
+//module.exports = {
+ //   addFn: add,
+//    minusFn: minus,
+//}
+
+exports.add = (a , b) => a + b;
+
+exports.sub = (a,b) => a-b;
